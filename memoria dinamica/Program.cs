64b5b1674e3e2df869860cb23f296bf8eb75c637
eherca0502 <ehc0502@outlook.com>
2023-08-24 22:10:17 -0600

    Agregar archivos de proyecto.

---
+++ memoria dinamica/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace memoria_dinamica
+{
+     class MemoriaD
+    {
+        static void Main()
+        {
+            // Cola = Estructura FIFO
+            Queue<int> cola =new Queue<int>();
+            cola.Enqueue(20);
+            cola.Enqueue(10);
+            cola.Enqueue(5);
+
+            while (cola.Count > 0)
+            {
+                int dato = cola.Dequeue();
+                Console.WriteLine(dato);
+            }
+
+        }
+    }
+}
